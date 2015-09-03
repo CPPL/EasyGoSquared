16d687b0e330b4d8a640505d5f8b3b5039f9af17
Craig Phillips <craig@craigphillips.biz>
2015-09-03 22:44:16 +1000

    Move supporting functions to helper class.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- plg_content_easygosquared/easygosquared.php
+++ plg_content_easygosquared/easygosquared.php
@@ -2,6 +2,8 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+require_once __DIR__ . '/easygosquaredhelper.php';
+
 class plgContentEasyGoSquared extends JPlugin
 {
     protected $trackAdmin;
@@ -27,7 +29,7 @@ class plgContentEasyGoSquared extends JPlugin
         // Load some settings
         $this->website_GoSqr_Token = $this->params->get('gosqr_token', 'GSN-000000-X');
         $this->trackAdmin = $this->params->get('track_admin', 0);
-        $this->weAreDoingIt = $this->allGood();
+        $this->weAreDoingIt = EasyGoSquaredHelper::allGood($this->trackAdmin);
     }
 
     /**
@@ -42,7 +44,7 @@ class plgContentEasyGoSquared extends JPlugin
     {
         if ($this->weAreDoingIt) {
             // Build the Script
-            $goScript = $this->buildGoSqrScript();
+            $goScript = EasyGoSquaredHelper::buildGoSqrScript($this->params, $this->website_GoSqr_Token);
 
             // Get current document and inject script
             $doc = JFactory::getDocument();
@@ -55,136 +57,5 @@ class plgContentEasyGoSquared extends JPlugin
         return true;
     }
 
-    private function buildGoSqrScript()
-    {
-        $gs_js = $this->gosqr_script($this->website_GoSqr_Token);
-
-        // Add any user properties that may be set
-        $gs_js .= $this->getUserPropertiesJS();
-
-        return $gs_js;
-    }
-
-    private function gosqr_script($token)
-    {
-        if ($token) {
-            $gs_js = <<<gs_js
-!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
-  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
-  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
-  insertBefore(d,q)}(window,document,'script','_gs');
-
-  _gs('$token');
-gs_js;
-        } else {
-            $gs_js = '';
-        }
-
-        return $gs_js;
-    }
-
-    /**
-     * Returns the User Properties setup in the plugin.
-     */
-    private function getUserPropertiesJS()
-    {
-        $user = JFactory::getUser();
-
-        $properties = explode(',', $this->params->get('user_properties_to_track', ''));
-
-        $userPropertiesJS = $this->buildGSUserProp($user, $properties);
-
-        return $userPropertiesJS;
-    }
-
-    private function buildGSUserProp($user, $properties = array())
-    {
-        $gsUserPropJS = '';
-
-        if (!$user->guest && is_array($properties) && count($properties)) {
-            $userProfile = JUserHelper::getProfile($user->id);
-            $profile = $userProfile->profile;
-
-            foreach ($properties as $propertyKey) {
-                if (isset($user->$propertyKey)) {
-                    $keyValue = $user->$propertyKey;
-
-                    switch ($propertyKey) {
-                        case 'groups':
-                            $keyValue = '\'' . $this->convertGroupIDsToNames($keyValue) . '\'';
-                            break;
-                        default:
-                            $keyValue = is_string($keyValue) && !is_int($keyValue) ? "'$keyValue'" : $keyValue;
-                    }
-
-                    $gsUserPropJS .= "$propertyKey: $keyValue, ";
-
-                } elseif (isset($profile[$propertyKey])) {
-                    $keyValue = $profile[$propertyKey];
-                    $keyValue = is_string($keyValue) && !is_int($keyValue) ? "'$keyValue'" : $keyValue;
-                    $gsUserPropJS .= "$propertyKey: $keyValue, ";
-                }
-            }
-
-            if ($gsUserPropJS != '') {
-                $gsUserPropJS = "_gs('identify', { $gsUserPropJS });";
-            }
-        }
-
-        return $gsUserPropJS;
-    }
-
-    private function convertGroupIDsToNames($usersGroups)
-    {
-        $groupNames = '';
-
-        if (is_array($usersGroups)) {
-            $allGroups = $this->getAllGroups();
-
-            // Look for matches to build our return string.
-            foreach ($allGroups as $group) {
-                if (in_array($group->id, $usersGroups)) {
-                    $groupNames .= $group->title . ',';
-                }
-            }
-
-            if ($groupNames != '' && substr($groupNames, strlen($groupNames) - 1, 1) == ',') {
-                $groupNames = substr($groupNames, 0, strlen($groupNames) - 1);
-            }
-
-        }
-
-        return $groupNames;
-    }
-
-    /**
-     * Simple bool check if we're enabled for backend or not in backend.
-     *
-     * @return bool
-     * @throws Exception
-     */
-    private function allGood()
-    {
-        $app = JFactory::getApplication();
-        $backEnd = $app->isAdmin();
-
-        return ($this->trackAdmin && $backEnd) || !$backEnd;
-    }
-
-    /**
-     * Get all group names
-     *
-     * @return mixed
-     */
-    private function getAllGroups()
-    {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true)
-            ->select('*')
-            ->from('#__usergroups');
-        $db->setQuery($query);
-        $allGroups = $db->loadObjectList();
-        return $allGroups;
-    }
 }
 
---
+++ plg_content_easygosquared/easygosquaredhelper.php
@@ -0,0 +1,145 @@
+<?php
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ *
+ * @author Craig Phillips
+ * @copyright Copyright (C) 2012 Craig Phillips Pty Ltd - All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE file
+ *
+ */
+class EasyGoSquaredHelper
+{
+    public static function buildGoSqrScript($params, $website_GoSqr_Token)
+    {
+        $gs_js = self::gosqr_script($website_GoSqr_Token);
+
+        // Add any user properties that may be set
+        $gs_js .= self::getUserPropertiesJS($params);
+
+        return $gs_js;
+    }
+
+    private static function gosqr_script($token)
+    {
+        if ($token) {
+            $gs_js = <<<gs_js
+!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
+
+  _gs('$token');
+gs_js;
+        } else {
+            $gs_js = '';
+        }
+
+        return $gs_js;
+    }
+
+    /**
+     * Returns the User Properties setup in the plugin.
+     */
+    private static function getUserPropertiesJS($params)
+    {
+        $user = JFactory::getUser();
+
+        $properties = explode(',', $params->get('user_properties_to_track', ''));
+
+        $userPropertiesJS = self::buildGSUserProp($user, $properties);
+
+        return $userPropertiesJS;
+    }
+
+    private static function buildGSUserProp($user, $properties = array())
+    {
+        $gsUserPropJS = '';
+
+        if (!$user->guest && is_array($properties) && count($properties)) {
+            $userProfile = JUserHelper::getProfile($user->id);
+            $profile = $userProfile->profile;
+
+            foreach ($properties as $propertyKey) {
+                if (isset($user->$propertyKey)) {
+                    $keyValue = $user->$propertyKey;
+
+                    switch ($propertyKey) {
+                        case 'groups':
+                            $keyValue = '\'' . self::convertGroupIDsToNames($keyValue) . '\'';
+                            break;
+                        default:
+                            $keyValue = is_string($keyValue) && !is_int($keyValue) ? "'$keyValue'" : $keyValue;
+                    }
+
+                    $gsUserPropJS .= "$propertyKey: $keyValue, ";
+
+                } elseif (isset($profile[$propertyKey])) {
+                    $keyValue = $profile[$propertyKey];
+                    $keyValue = is_string($keyValue) && !is_int($keyValue) ? "'$keyValue'" : $keyValue;
+                    $gsUserPropJS .= "$propertyKey: $keyValue, ";
+                }
+            }
+
+            if ($gsUserPropJS != '') {
+                $gsUserPropJS = "_gs('identify', { $gsUserPropJS });";
+            }
+        }
+
+        return $gsUserPropJS;
+    }
+
+    private static function convertGroupIDsToNames($usersGroups)
+    {
+        $groupNames = '';
+
+        if (is_array($usersGroups)) {
+            $allGroups = self::getAllGroups();
+
+            // Look for matches to build our return string.
+            foreach ($allGroups as $group) {
+                if (in_array($group->id, $usersGroups)) {
+                    $groupNames .= $group->title . ',';
+                }
+            }
+
+            if ($groupNames != '' && substr($groupNames, strlen($groupNames) - 1, 1) == ',') {
+                $groupNames = substr($groupNames, 0, strlen($groupNames) - 1);
+            }
+        }
+
+        return $groupNames;
+    }
+
+    /**
+     * Simple bool check if we're enabled for backend or not in backend.
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public static function allGood($trackAdmin)
+    {
+        $app = JFactory::getApplication();
+        $backEnd = $app->isAdmin();
+
+        return ($trackAdmin && $backEnd) || !$backEnd;
+    }
+
+    /**
+     * Get all group names
+     *
+     * @return mixed
+     */
+    private static function getAllGroups()
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true)
+            ->select('*')
+            ->from('#__usergroups');
+        $db->setQuery($query);
+        $allGroups = $db->loadObjectList();
+        return $allGroups;
+    }
+
+}
